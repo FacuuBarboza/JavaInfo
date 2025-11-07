415d0000dfeb573647cabd9cdc85769c3e245549
FacuuBarboza <facu.mbarboza@gmail.com>
2025-11-07 19:05:50 -0300

    Se agrega el package dto para el guardado de estadisticas

---
+++ src/main/java/tpijava/dto/ReporteDTO.java
@@ -0,0 +1,23 @@
+package tpijava.dto;
+
+public class ReporteDTO {
+    private double promedioDuracion;
+    private double porcentajeExito;
+    private int totalExitos;
+    private int totalFallos;
+    private int totalExperimentos;
+
+    public ReporteDTO(double promedioDuracion, double porcentajeExito, int totalExitos, int totalFallos, int totalExperimentos) {
+        this.promedioDuracion = promedioDuracion;
+        this.porcentajeExito = porcentajeExito;
+        this.totalExitos = totalExitos;
+        this.totalFallos = totalFallos;
+        this.totalExperimentos = totalExperimentos;
+    }
+
+    public double getPromedioDuracion() { return promedioDuracion; }
+    public double getPorcentajeExito() { return porcentajeExito; }
+    public int getTotalExitos() { return totalExitos; }
+    public int getTotalFallos() { return totalFallos; }
+    public int getTotalExperimentos() { return totalExperimentos; }
+}
